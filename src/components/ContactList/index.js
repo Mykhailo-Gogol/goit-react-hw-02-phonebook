ea12b839f5496eb46b022e864ea0a7eca4f2a3fc
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-03-26 12:34:20 +0200

    Змінює ContactList, Filter на функціональні компоненти

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,1 +1,47 @@
-export { default } from "./ContactList";
+import shortid from "shortid";
+
+const ContactList = (props) => {
+  const uniqueId = () => {
+    return shortid.generate();
+  };
+
+  const contactFilter = () => {
+    const { filter, contacts } = props.appState;
+    const normalizedFilter = filter.toLowerCase();
+    const filteredArray = contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(normalizedFilter) ||
+        number.toLowerCase().includes(normalizedFilter)
+    );
+    return filteredArray;
+  };
+
+  return (
+    <>
+      {contactFilter().length ? (
+        <ul className="contact-list">
+          {contactFilter().map(({ name, number, id }) => {
+            return (
+              <li key={uniqueId()}>
+                <p className="contact-text">
+                  <span>{name}</span>
+                  <span>{number}</span>
+                </p>
+                <button
+                  className="delete-button"
+                  onClick={() => props.onDeleteContact(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>Contact list is empty</p>
+      )}
+    </>
+  );
+};
+
+export default ContactList;
--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,1 +1,38 @@
-export { default } from "./Filter.js";
+import { useState } from "react";
+const Filter = (props) => {
+  const [filter, setFilter] = useState("");
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setName(`${name}`);
+    setValue(value);
+  };
+
+  const filterInputHandler = (event) => {
+    handleInputChange(event);
+    props.onFilter(filter);
+  };
+
+  const blurHandler = (event) => {
+    event.target.value = "";
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="search contact"
+        name="filter"
+        value={filter}
+        onChange={filterInputHandler}
+        onBlur={blurHandler}
+      />
+    </div>
+  );
+};
+
+export default Filter;
